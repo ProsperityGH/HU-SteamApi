06516d14660c8eed9b0cd545afb1258b6cdef330
Jorn <jbransen2006@gmail.com>
2024-11-22 13:51:14 +0100

    Like to dislike ratio in een functie gestopt

--- AI.py
+++ AI.py
@@ -1,14 +1,18 @@
 import json
 
-with open('steam.json', 'r') as json_file:
-    data = json.load(json_file)
+def like_to_dislike(game_name):
+    with open('steam.json', 'r') as json_file: # Opent json file onder de naam 'json_file'
+        data = json.load(json_file)            # Zet data als alles in de json file, als je data print dan zie je alles in de json
 
-for game in data:
-    game_lst = []
-    print(game['name'])
-    positive = game['positive_ratings']
-    negative = game['negative_ratings']
-    game_lst.append(positive), game_lst.append(negative)
-    no_brackets = str(game_lst)[1:-1]
+    for game in data:                          # Doorloopt elke game in de json
+        game_lst = []
+        if game_name == game['name']:
+            print(game['name'])
+            positive = game['positive_ratings']
+            negative = game['negative_ratings']
+            game_lst.append(positive), game_lst.append(negative)
+            no_brackets = str(game_lst)[1:-1]
+            percentage = ((negative-positive) / positive) * -100
+            print(f'Like to dislike ratio = {no_brackets.replace(',', ' -')} ({round(percentage,2)}%)')
 
-    print(f'Like to dislike ratio = {no_brackets.replace(',', ' -')}')
+like_to_dislike('Half-Life: Blue Shift')
