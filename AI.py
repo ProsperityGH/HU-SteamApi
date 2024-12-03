8264cafd2525d172b8de6be4aeb81c88dd909b77
Jorn <jbransen2006@gmail.com>
2024-12-03 09:01:10 +0100

    Genre functie en genre filter om de mediaan en het gemiddelde van de speeltijd van een bepaalde genre te vinden,

--- AI.py
+++ AI.py
@@ -3,25 +3,51 @@ import json
 with open('steam.json', 'r') as json_file:  # Opent json file onder de naam 'json_file'
     data = json.load(json_file)  # Zet data als alles in de json file, als je data print dan zie je alles in de json
 
-def mediaan():
-    playtime_list = []
-    swapped = True
+def genres():
+    genre_list = set() # Maak een set, zodat het makkelijker is om te checken of data al in de set staat
 
-    for median in data:
-        playtime_list.append(median['median_playtime'])
+    for file in data:
+        for genre in file["genres"].split(';'):
+            genre_list.add(genre.strip())
+
+    return genre_list
+
+
+def mediaan_en_gemiddelde():
+    playtime_list = []
+    average_list = []
+
+    print(genres())
+    genre = str(input(f"Choose a genre from the list above: "))
+
+    for playtime in data:
+        if genre in playtime["genres"].split(";"): # Checkt of de gekozen genre in de game zit, zo ja dan word hij aan de lijst toegevoegd
+            playtime_list.append(playtime['median_playtime'])
+            average_list.append(playtime['average_playtime'])
+
+    if not playtime_list:
+        print("No playtimes found for the selected genre.")
+        return None, None
 
     x = len(playtime_list)-1
     y = x / 2
+    swapped = True
 
-    while swapped:
+    while swapped: # Sorteert de lijst
         swapped = False
-        for i in range(x):
+        for i in range(x): # Sorteer playtime_list
             if playtime_list[i] > playtime_list[i+1]:
                 playtime_list[i], playtime_list[i+1] = playtime_list[i+1], playtime_list[i]
                 swapped = True
+        for e in range(x): # Sorteer average_list
+            if average_list[e] > average_list[e+1]:
+                average_list[e], average_list[e+1] = average_list[e+1], average_list[e]
+                swapped = True
 
-    print(playtime_list)
-    return playtime_list[int(y)]
+    gemiddelde = sum(average_list)/len(average_list)
+
+    return playtime_list[int(y)], round(gemiddelde, 2)
+
 
 
 def search(letter):
@@ -42,4 +68,4 @@ def search(letter):
     return gamelist
 
 # print(search('b'))
-print(mediaan())
+print(mediaan_en_gemiddelde())
